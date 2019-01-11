f0c1a17e5184b0fe028f7ee3da99d48851f08adb
ueta-eisuke <ueta.eisuke1@gmail.com>
2019-01-11 16:10:46 +0700

    use inputs.json as parameter

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import json
+from typing import List
+
 from app import Task
 from framework import Context
 
+DAG_ID = "dag_id"
+
+def read_inputs_json(context: Context) -> List:
+    path = context.config.get('inputs')[0]
+    inputs = []
+    with open(path) as f:
+        inputs = json.load(f)
+    return inputs
+
 
 def execute() -> None:
-    dag_id = 'dag_id'
-    context = Context(dag_id)
+    context = Context(DAG_ID)
     task = Task(context)
-    inputs = context.config.get('inputs')
+    inputs = read_inputs_json(context)
     task.execute(inputs)
-
 
 if __name__ == "__main__":
     execute()
