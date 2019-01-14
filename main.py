ee50f3c272e83cfac0ecb3ea68a0198c981640a9
ueta-eisuke <ueta.eisuke1@gmail.com>
2019-01-14 10:16:36 +0700

    add resoruce_id in main.py

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ from app import Task
 from framework import Context
 
 DAG_ID = "dag_id"
+RESOURCE_ID = "resource_id"
 
 def read_inputs_json(context: Context) -> List:
     path = context.config.get('inputs')[0]
-    inputs = []
+    contents = []
     with open(path) as f:
-        inputs = json.load(f)
+        contents = json.load(f)
+    return contents
+
+def generate_inputs(context: Context) -> List:
+    contents = read_inputs_json(context)
+    inputs = []
+    for index, content in enumerate(contents):
+        input = {
+            "resource_id": RESOURCE_ID + "_" + str(index),
+            "content": content
+        }
+        inputs.append(input)
     return inputs
 
 
 def execute() -> None:
     context = Context(DAG_ID)
     task = Task(context)
-    inputs = read_inputs_json(context)
+    inputs = generate_inputs(context)
     task.execute(inputs)
 
 if __name__ == "__main__":
