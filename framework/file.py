5673520f687c7fb530ec65c3205579925ac8a5f0
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-01-15 10:11:27 +0700

    Locate "data" and "tmp" directory under "shared" directory

--- framework/file.py
+++ framework/file.py
@@ -5,28 +5,30 @@ class File(object):
     """
     File utility class.
     """
-    DATA_DIR = 'data' + os.path.sep
-    TMP_DIR = 'tmp' + os.path.sep
 
     def __init__(self):
         self.root_path = self._get_root_path()
 
+    def get_shared_path(self, path: str = '') -> str:
+        """
+        Returns absolute path to `shared` directory.
+        """
+        return os.path.abspath(os.path.join(self.root_path, 'shared', path))
+
     def get_data_path(self, path: str = '') -> str:
         """
-        Returns absolute path to `data` directory.
+        Returns absolute path to `shared/data` directory.
         """
-        return os.path.abspath(self.root_path + self.DATA_DIR + path)
+        return os.path.abspath(os.path.join(self.get_shared_path(), 'data', path))
 
     def get_tmp_path(self, path: str = '') -> str:
         """
-        Returns absolute path to `tmp` directory.
+        Returns absolute path to `shared/tmp` directory.
         """
-        return os.path.abspath(self.root_path + self.TMP_DIR + path)
+        # return os.path.abspath(self.root_path + self.TMP_DIR + path)
+        return os.path.abspath(os.path.join(self.get_shared_path(), 'tmp', path))
 
     def _get_root_path(self) -> str:
         if not os.environ.get("POC_BASE_ROOT"):
             return ""
-        root_path = os.environ.get("POC_BASE_ROOT")
-        if not root_path.endswith('/'):
-            root_path += '/'
-        return root_path
+        return os.environ.get("POC_BASE_ROOT")
