d39659d7da2d76a6612bc0ef1c2f880a9bde8f2a
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-01-16 10:02:10 +0700

    Refactor framework/file.py

--- framework/file.py
+++ framework/file.py
@@ -7,7 +7,7 @@ class File(object):
     """
 
     def __init__(self):
-        self.root_path = self._get_root_path()
+        self.root_path = os.environ.get("POC_BASE_ROOT") if os.environ.get("POC_BASE_ROOT") else ''
 
     def get_shared_path(self, path: str = '') -> str:
         """
@@ -19,16 +19,10 @@ class File(object):
         """
         Returns absolute path to `shared/data` directory.
         """
-        return os.path.abspath(os.path.join(self.get_shared_path(), 'data', path))
+        return os.path.abspath(os.path.join(self.root_path, 'shared', 'data', path))
 
     def get_tmp_path(self, path: str = '') -> str:
         """
         Returns absolute path to `shared/tmp` directory.
         """
-        # return os.path.abspath(self.root_path + self.TMP_DIR + path)
-        return os.path.abspath(os.path.join(self.get_shared_path(), 'tmp', path))
-
-    def _get_root_path(self) -> str:
-        if not os.environ.get("POC_BASE_ROOT"):
-            return ""
-        return os.environ.get("POC_BASE_ROOT")
+        return os.path.abspath(os.path.join(self.root_path, 'shared', 'tmp', path))
