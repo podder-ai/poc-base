46f59bb10ba0dc3d9fc59c907a96a2f4615b8fd5
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-02-01 14:29:59 +0700

    Remove api modules

--- run_codegen.py
+++
@@ -1,9 +0,0 @@
-from grpc_tools import protoc
-
-protoc.main((
-    '',
-    '-I./api/protos',
-    '--python_out=./api',
-    '--grpc_python_out=./api',
-    './api/protos/pipeline_framework.proto',
-))
