75794767cbe191f1b728d11c13f2dc191dffdd79
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-02-04 14:09:05 +0700

    Add necessary api files

--- api/grpc_server.py
+++ api/grpc_server.py
@@ -5,7 +5,9 @@ DO NOT MODIFY THIS FILE.
 import os
 
 from app import Task
-from podder_task_base.api.grpc_server import run_grpc_server
+from podder_task_base.api.grpc_server import GrpcServer
+from podder_task_base.api.task_api import PocBaseApi
+from protos import pipeline_framework_pb2_grpc
 
 DEFAULT_GRPC_PID_FILE = "/var/run/poc_base.pid"
 DEFAULT_PORT = 50051
@@ -20,5 +22,8 @@ if __name__ == '__main__':
     pidfile = os.getenv("GRPC_PID_FILE", DEFAULT_GRPC_PID_FILE)
     max_workers = os.getenv("GRPC_MAX_WORKERS", DEFAULT_MAX_WORKERS)
     port = os.getenv("GRPC_PORT", DEFAULT_PORT)
+    add_servicer_method = pipeline_framework_pb2_grpc.add_PocBaseApiServicer_to_server
+    task_api_class = PocBaseApi
 
-    run_grpc_server(stdout, stderr, pidfile, Task, max_workers, port)
+    GrpcServer(stdout, stderr, pidfile, Task, max_workers, port,
+               add_servicer_method, task_api_class).run()
