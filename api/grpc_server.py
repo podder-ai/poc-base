23319a05521d860a539194f2e1064463745a50f4
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-02-04 10:04:28 +0700

    Simplify grpc_server.py

--- api/grpc_server.py
+++ api/grpc_server.py
@@ -1,18 +1,17 @@
+"""
+DO NOT MODIFY THIS FILE.
+"""
+
 import os
 
-import daemon
-from daemon import pidfile
-
-GRPC_PID_FILE = os.environ.get("GRPC_PID_FILE")
-
-from podder_task_base.api.grpc_server import serve
 from app import Task
+from podder_task_base.api.grpc_server import run_grpc_server
 
 if __name__ == '__main__':
+    """
+    Run gRPC server.
+    """
     stdout = open(os.environ.get("GRPC_LOG"), 'a')
     stderr = open(os.environ.get("GRPC_ERROR_LOG"), 'a')
-    pidfile = daemon.pidfile.PIDLockFile(GRPC_PID_FILE)
-    with daemon.DaemonContext(
-            stdout=stdout, stderr=stderr, pidfile=pidfile,
-            detach_process=True):
-        serve(execution_task=Task)
+    pidfile = os.environ.get("GRPC_PID_FILE")
+    run_grpc_server(stdout, stderr, pidfile, Task)
