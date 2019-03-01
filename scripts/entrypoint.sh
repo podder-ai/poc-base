2ac6ba6cd43d676db3fd692f3ed19621883848c7
Takayoshi Nishida <takayoshi.nishida@gmail.com>
2019-03-01 18:28:46 +0700

    Restart gRPC server when it's pid file exists

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-rm -f $GRPC_PID_FILE
-python api/grpc_server.py; tail -f $GRPC_LOG & tail -f $GRPC_ERROR_LOG
+echo "Starting process..."
+
+kill_grpc_process() {
+    echo "gRPC pid file already exists."
+    ps -ef | grep python | grep -v grep | awk '{print $2}' | xargs kill
+    rm -f $GRPC_PID_FILE
+    echo "Stopped existing gRPC process."
+}
+
+if [ -f $GRPC_PID_FILE ]; then
+    kill_grpc_process
+    echo "Restarting gRPC server..."
+    python api/grpc_server.py
+else
+    echo "Starting gRPC server..."
+    python api/grpc_server.py; tail -f $GRPC_LOG & tail -f $GRPC_ERROR_LOG
+fi
